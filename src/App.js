90dedeaf8813a333ad24c7d95864fc1dc2162f92
Jorge Echeverria <coko.eche@gmail.com>
2023-01-04 11:54:50 +1100

    delete Layout component, put content instead  inside App component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import { useState } from "react";
-import Layout from "./components/Layout";
+
+import Sidebar from "./components/Sidebar";
+import Home from "./components/Home";
+
 import "./App.css";
 import { createGlobalStyle } from "styled-components";
 import styled from "styled-components";
@@ -29,30 +32,75 @@ body {
 }
 `;
 
-// const Sections = styled.div`
-//   width: 100%;
-//   height: 90%;
-//   min-height: 566px;
-//   position: absolute;
-//   top: 19%;
-//   left: 8%;
-//   margin: 0 auto;
-//   z-index: 1;
-//   transform-style: preserve-3d;
-//   animation: fadeIn 1s forwards;
-//   animation-delay: 1s;
-// `;
+const Page = styled.div`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  overflow: hidden;
+`;
+
+const Tag = styled.span`
+  font-family: "La Belle Aurore", cursive;
+  font-size: 1.125rem;
+  color: var(--light-yellow);
+  left: 120px;
+  margin-left: ${(props) =>
+    props.leftmargin === "negative"
+      ? "-20px"
+      : props.leftmargin === "positive"
+      ? "20px"
+      : ""};
+`;
+
+const TopTag = styled(Tag)`
+  position: absolute;
+  top: 35px;
+  left: 100px;
+  opacity: 0.6;
+`;
+
+const BottomTag = styled(Tag)`
+  position: absolute;
+  bottom: 0;
+  opacity: 0.6;
+`;
+
+const Section = styled.div`
+  width: 100%;
+  height: 90%;
+  min-height: 566px;
+  position: absolute;
+  top: 19%;
+  left: 8%;
+  margin: 0 auto;
+  z-index: 1;
+  transform-style: preserve-3d;
+  animation: fadeIn 1s forwards;
+  animation-delay: 1s;
+`;
 
 function App() {
-  const [isHome, setIsHome] = useState(true);
-  const [isAbout, setIsAbout] = useState(false);
-  const [isPortfolio, setIsPortfolio] = useState(false);
-  const [isContact, setIsContact] = useState(false);
-
   return (
     <>
       <GlobalStyle />
-      <Layout />
+      <Sidebar />
+      <Page>
+        <TopTag>
+          &lt;html&gt;
+          <br />
+          <Tag leftmargin="positive">&lt;body&gt;</Tag>
+        </TopTag>
+
+        <Section>
+          <Home />
+        </Section>
+
+        <BottomTag>
+          &lt;/body&gt;
+          <br />
+          <Tag leftmargin="negative">&lt;/html&gt;</Tag>
+        </BottomTag>
+      </Page>
     </>
   );
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,16 +1,14 @@
 import styled from "styled-components";
 
-const Page = styled.div``;
-
 const Home = () => {
   return (
-    <Page>
+    <>
       <h1>
         Hi!
         <br /> My name is <span>Jorge</span> Echeverria{" "}
       </h1>
       <h2>I am a Frontend Developer</h2>
-    </Page>
+    </>
   );
 };
 
