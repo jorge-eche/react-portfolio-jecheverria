dcad1da5e696981886a56c78ed01b64a54e0e262
Jorge Echeverria <coko.eche@gmail.com>
2023-01-08 13:18:56 +1100

    build layout of About component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import JorgePic from "../images/je2.jpg";
 
 const Title1 = styled.h1`
   font-size: 3.3rem;
@@ -6,6 +7,7 @@ const Title1 = styled.h1`
   font-family: "Coolvetica", Helvetica;
   font-weight: 400;
   margin-left: -40px;
+  color: var(--light-yellow);
 
   &:before {
     content: "<h1>";
@@ -30,8 +32,45 @@ const Title1 = styled.h1`
   }
 `;
 
+const Paragraph = styled.p`
+  margin: 40px 100px;
+`;
+
+const Resume = styled.button``;
+
 const About = () => {
-  return <Title1>About Me</Title1>;
+  return (
+    <>
+      {" "}
+      <Title1>About Me</Title1>
+      <div>
+        {" "}
+        <div>
+          {" "}
+          <Paragraph>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem,
+            blanditiis quam! Tempora officia iure ullam corrupti numquam quas
+            mollitia voluptate odio perferendis molestias ipsum cum, dolores
+            laudantium. Repellendus, at ut!
+          </Paragraph>
+          <Paragraph>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem,
+            blanditiis quam! Tempora officia iure ullam corrupti numquam quas
+            mollitia voluptate odio perferendis molestias ipsum cum, dolores
+            laudantium. Repellendus, at ut!
+          </Paragraph>
+          <Paragraph>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem,
+            blanditiis quam! Tempora officia iure ullam corrupti numquam quas
+            mollitia voluptate odio perferendis molestias ipsum cum, dolores
+            laudantium. Repellendus, at ut!
+          </Paragraph>
+        </div>
+        <img src={JorgePic} alt="Jorge Picture" />
+      </div>
+      <button>Resume</button>
+    </>
+  );
 };
 
 export default About;
