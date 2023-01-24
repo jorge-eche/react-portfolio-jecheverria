411dcb5a67faec4effb7f4bdcc3a2f8d2fc13f42
Jorge Echeverria <coko.eche@gmail.com>
2023-01-24 12:53:44 +1100

    add animation fadeIn to Nav mobile menu

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -53,17 +53,27 @@ const Container = styled.div`
     `@media screen and (max-width: 1200px) {
       overflow: hidden;
       background-color: var(--dark-red);
+      opacity: 0;
       z-index: 1;
       position: absolute;
       top: 0;
       bottom: 0;
       left: 0;
-      right: 0;
+      right: 0; 
       padding: 10px 10px 0;
       align-items: flex-start;
       flex-direction: column;
       justify-content: flex-start;
       gap: 10px;
+      animation: fadeIn 1s forwards;
+      @keyframes fadeIn {
+        from {
+          opacity: 0;
+        }
+        to {
+          opacity: 1;
+        }
+      }
 
       ${JELogo} {
         display:none;
